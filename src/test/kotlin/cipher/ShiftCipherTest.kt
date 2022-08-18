ab469bb43b4f94239b86d82d47af22d2ef3be847
Elián <elian.fabian.el.belaizi@gmail.com>
2022-08-18 22:06:21 +0200

    Add more tests to ShiftCipher and refactor

--- src/test/kotlin/cipher/ShiftCipherTest.kt
+++ src/test/kotlin/cipher/ShiftCipherTest.kt
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test
 
 internal class ShiftCipherTest
 {
-    private lateinit var cipher: ShiftCipher
+    private lateinit var morseCipher: ShiftCipher
+    private lateinit var alphabetCipher: ShiftCipher
 
     private val morseSet = setOf(
         ".-", "-...", "-.-.", "-..", ".",
@@ -18,42 +19,83 @@ internal class ShiftCipherTest
         "-.--", "--.."
     )
 
-    private val morseString = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --.-- --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.."
-    private val shiftedMorseString = "-... -.. ..-. . --. .. .... .--- .-.. -.- -- --.-- -. --- --.- .--. .-. - ... ..- .-- ...- -..- --.. -.-- .- -.-."
+    private val alphabetSet = setOf(
+        "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+        "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s", "t",
+        "u", "v", "w", "x", "y", "z"
+    )
+
+    private val morseString = ".- -... -.-. -.. . ..-. --. .... .. .--- -.- .-.. -- -. --.-- --- .--. --.- .-. ... - ..- ...- .-- -..- -.-- --.. / .- -. -.. / --- -..- --- ..-. . -- .--. .- .-.."
+    private val shiftedMorseString = "-... -.. ..-. . --. .. .... .--- .-.. -.- -- --.-- -. --- --.- .--. .-. - ... ..- .-- ...- -..- --.. -.-- .- -.-. / -... --- --. / .--. --.. .-. --. --. --- --.- -.-. --.--"
+
+    private val alphabetString = "abcdefghijklmnñopqrstuvwxyz and oxofempal"
+    private val shiftedAlphabetString = "bdfegihjlkmñnoqprtsuwvxzyac bog pzrggoqcñ"
 
     @BeforeEach
     fun onBefore()
     {
-        cipher = ShiftCipher(
+        morseCipher = ShiftCipher(
             morseSet,
             wordSeparator = "/",
             symbolSeparator = " ",
             cipherKey = ".- -... -.-."
         )
+
+        alphabetCipher = ShiftCipher(
+            alphabetSet,
+            wordSeparator = " ",
+            symbolSeparator = "",
+            cipherKey = "abc"
+        )
     }
 
     @Test
-    fun `Decode an encoded string must be equal to the original one`()
+    fun `Morse - Decode an encoded string must be equal to the original one`()
     {
-        val encodedString = cipher.encode(morseString)
-        val decodedString = cipher.decode(encodedString)
+        val encodedString = morseCipher.encode(morseString)
+        val decodedString = morseCipher.decode(encodedString)
 
         assertThat(decodedString).isEqualTo(morseString)
     }
 
     @Test
-    fun `Encode is correct`()
+    fun `Morse - Encode is correct`()
     {
-        val encodedString = cipher.encode(morseString)
+        val encodedString = morseCipher.encode(morseString)
 
         assertThat(encodedString).isEqualTo(shiftedMorseString)
     }
 
     @Test
-    fun `Decode is correct`()
+    fun `Morse - Decode is correct`()
     {
-        val decodedString = cipher.decode(shiftedMorseString)
+        val decodedString = morseCipher.decode(shiftedMorseString)
 
         assertThat(decodedString).isEqualTo(morseString)
+    }
+
+    @Test
+    fun `Alphabet - Decode an encoded string must be equal to the original one`()
+    {
+        val encodedString = alphabetCipher.encode(alphabetString)
+        val decodedString = alphabetCipher.decode(encodedString)
+
+        assertThat(decodedString).isEqualTo(alphabetString)
+    }
+
+    @Test
+    fun `Alphabet - Encode is correct`()
+    {
+        val encodedString = alphabetCipher.encode(alphabetString)
+
+        assertThat(encodedString).isEqualTo(shiftedAlphabetString)
+    }
+
+    @Test
+    fun `Alphabet - Decode is correct`()
+    {
+        val decodedString = alphabetCipher.decode(shiftedAlphabetString)
+
+        assertThat(decodedString).isEqualTo(alphabetString)
     }
 }
